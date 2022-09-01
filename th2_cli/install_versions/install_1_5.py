05a2c7fcd4e0ec9288c56313578de47ec873b48d
nikolay.dorofeev <dorich2000@gmail.com>
2022-09-01 11:42:22 +0000

    Exceptions for flannel and namespaces

--- th2_cli/install_versions/install_1_5.py
+++ th2_cli/install_versions/install_1_5.py
@@ -1,6 +1,7 @@
 from kubernetes import client, config
 from kubernetes.utils import create_from_yaml
 from th2_cli.utils import get_file
+import yaml
 
 config.load_kube_config()
 k8s_client = client.ApiClient()
@@ -9,8 +10,22 @@ k8s_core = client.CoreV1Api()
 
 def install_1_5():
     print("Preparing Kubernetes cluster...")
-    flannel_manifest = get_file(
-        "https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml")
-    create_from_yaml(k8s_client=k8s_client, yaml_file=flannel_manifest)
-    k8s_core.create_namespace(client.V1Namespace(metadata=client.V1ObjectMeta(name="monitoring")))
-    k8s_core.create_namespace(client.V1Namespace(metadata=client.V1ObjectMeta(name="service")))
+    # Install flannel
+    try:
+        flannel_manifest = get_file(
+            "https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml")
+        create_from_yaml(k8s_client=k8s_client, yaml_objects=yaml.safe_load_all(flannel_manifest))
+        print('flannel-cni is installed')
+    except:
+        print("Error while installing flannel. It might exist")
+    # Create namespaces
+    try:
+        k8s_core.create_namespace(client.V1Namespace(metadata=client.V1ObjectMeta(name="monitoring")))
+        print('monitoring namespace created')
+    except:
+        print('monitoring namespace already exist')
+    try:
+        k8s_core.create_namespace(client.V1Namespace(metadata=client.V1ObjectMeta(name="service")))
+        print('service namespace created')
+    except:
+        print('service namespace already exist')
