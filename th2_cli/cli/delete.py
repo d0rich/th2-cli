d38645db1823dd9fbb562ad6be5facda69770a71
nikolay.dorofeev <dorich2000@gmail.com>
2022-09-26 11:08:13 +0000

    [TH2CLI-6] Delete th2 feature completed

--- th2_cli/cli/delete.py
+++ th2_cli/cli/delete.py
@@ -1,6 +1,6 @@
-from th2_cli.utils.infra import pre_delete_warning, get_infra_mgr_config
+from th2_cli.utils.infra import pre_delete_warning, get_infra_mgr_config, delete_namespace
 from th2_cli.utils import read_value, print_info, print_used_value
-from th2_cli.utils.kubernetes import connect
+from th2_cli.utils.kubernetes import connect, get_namespaces
 
 
 def delete():
@@ -8,5 +8,14 @@ def delete():
     print_info('Connecting to the cluster...')
     k8s_client, k8s_core = connect()
     prefix = get_infra_mgr_config(k8s_core)['kubernetes']['namespacePrefix']
-    print(prefix)
+    th2_namespaces = filter(lambda ns: ns.startswith(prefix), get_namespaces(k8s_core))
+    print_info('Deleting "monitoring" namespace...')
+    delete_namespace(k8s_core, 'monitoring')
+    print_info('Deleting "service" namespace...')
+    delete_namespace(k8s_core, 'service')
+    print_info('Deleting th2 namespaces...')
+    for ns in th2_namespaces:
+        print_info(f'Deleting {ns} namespace...')
+        delete_namespace(k8s_core, ns)
+    print_info('th2 is deleted from your Kubernetes cluster')
 
