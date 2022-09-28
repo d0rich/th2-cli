a6fc5e08f028adb64ab52f036ec267c24d1094db
nikolay.dorofeev <dorich2000@gmail.com>
2022-09-28 12:39:03 +0000

    Spinner for th2 deletion

--- th2_cli/cli/delete.py
+++ th2_cli/cli/delete.py
@@ -1,6 +1,8 @@
 from th2_cli.utils.infra import pre_delete_warning, get_infra_mgr_config, delete_namespace, delete_pv
 from th2_cli.utils import read_value, print_info, print_used_value
 from th2_cli.utils.kubernetes import connect, get_namespaces, get_pvs
+from halo import Halo
+import time
 
 
 def delete():
@@ -8,11 +10,9 @@ def delete():
     print_info('Connecting to the cluster...')
     k8s_client, k8s_core = connect()
     prefix = get_infra_mgr_config(k8s_core)['kubernetes']['namespacePrefix']
-    th2_namespaces = filter(lambda ns: ns.startswith(prefix), get_namespaces(k8s_core))
-    print_info('Deleting "monitoring" namespace...')
-    delete_namespace(k8s_core, 'monitoring')
-    print_info('Deleting "service" namespace...')
-    delete_namespace(k8s_core, 'service')
+    th2_namespaces = list(filter(
+        lambda ns: ns.startswith(prefix) or ns in ['service', 'monitoring'],
+        get_namespaces(k8s_core)))
     print_info('Deleting th2 namespaces...')
     for ns in th2_namespaces:
         print(f'  Deleting {ns} namespace...')
@@ -21,5 +21,13 @@ def delete():
     for pv in get_pvs(k8s_core):
         print(f'  Deleting {pv} PersistentVolume...')
         delete_pv(k8s_core, pv)
+    spinner = Halo(text='Waiting until all th2 namespaces are terminated...', spinner='dots')
+    spinner.start()
+    while len(th2_namespaces) != 0:
+        th2_namespaces = list(filter(
+            lambda ns: ns.startswith(prefix) or ns in ['service', 'monitoring'],
+            get_namespaces(k8s_core)))
+        time.sleep(1)
+    spinner.stop()
     print_info('th2 is deleted from your Kubernetes cluster')
 
