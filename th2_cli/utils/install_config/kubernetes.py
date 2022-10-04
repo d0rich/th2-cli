e1897f5b57e4681f515b1ba4c49c05e530438449
nikolay.dorofeev <dorich2000@gmail.com>
2022-10-04 15:29:07 +0000

    Default values methods

--- th2_cli/utils/install_config/kubernetes.py
+++ th2_cli/utils/install_config/kubernetes.py
@@ -1,13 +1,21 @@
 from dataclasses import dataclass
 from th2_cli.utils import is_ip
 
+NODE_DEFAULT_VALUE = HOST_DEFAULT_VALUE = ''
+
 
 @dataclass
 class KubernetesConfigSection:
-    pvs_node: str = ''
-    host: str = ''
+    pvs_node: str = NODE_DEFAULT_VALUE
+    host: str = HOST_DEFAULT_VALUE
 
     def hostname(self) -> str:
         if is_ip(self.host):
             return ''
         return self.host
+
+    def pvs_node_is_default(self) -> bool:
+        return self.pvs_node == NODE_DEFAULT_VALUE
+
+    def host_is_default(self) -> bool:
+        return self.host == HOST_DEFAULT_VALUE
