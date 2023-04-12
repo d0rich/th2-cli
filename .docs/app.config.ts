a322cb9066e2d2707f58ee19cc4614f00240902d
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-12 20:26:18 +0400

    Init d0xigen

---
+++ .docs/app.config.ts
@@ -0,0 +1,23 @@
+export default defineAppConfig({
+  d0xigen: {
+    title: 'th2-cli',
+    description: '👨‍💻 CLI for managing th2 infrastructure in Kubernetes cluster',
+    url: 'https://th2-cli.d0rich.me',
+    author: 'Nikolay Dorofeev',
+    social: {
+      github: 'https://github.com/d0rich/th2-cli'
+    },
+    features: {
+      gtag: {
+        id: 'G-7WPZF124G5'
+      }
+    },
+    seo: {
+      keywords: [ 'Python', 'cli', 'DevOps', 'Kubernetes', 'Helm' ]
+    },
+    d0richIndex: {
+      complexity: 3,
+      tags: [ 'cli', 'microservices', 'dev-ops' ]
+    }
+  }
+})
